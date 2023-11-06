2bfbaf19269488817dba8b30dd245cbac357de1b
cameronmcewan <cm17521@bristol.ac.uk>
2023-11-06 01:18:10 +0000

    update deployWithEthers.ts and CastVote.ts

--- scripts/CastVote.ts
+++ scripts/CastVote.ts
@@ -4,34 +4,40 @@ dotenv.config();
 import { Ballot, Ballot__factory } from "../typechain-types";
 
 
-async function main() 
-{ 
-    // Receiving the parameters
+async function main() { 
+    // Receiving parameters
     const parameters = process.argv.slice(2);
     if (!parameters || parameters.length < 2)
-    throw new Error("Parameters not provided");
+        throw new Error("Parameters not provided");
     const contractAddress = parameters[0];
-    const proposlaNumber = parameters[1];
+    const proposalNumber = parameters[1];
+
+    // Configuring the provider 
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+    const lastBlock = await provider.getBlock("latest");
+    console.log(`Last block number: ${lastBlock?.number}`);
+    const lastBlockTimestamp = lastBlock?.timestamp ?? 0;
+    const lastBlockDate = new Date(lastBlockTimestamp * 1000);
+    console.log(
+        `Last block timestamp: ${lastBlockTimestamp} (${lastBlockDate.toLocaleDateString})`
+    );
 
     // Configuring the wallet
-    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
     console.log(`Using address ${wallet.address}`);
     const balanceBN = await provider.getBalance(wallet.address);
     const balance = Number(ethers.formatUnits(balanceBN));
-    console.log(`Wallet balance ${balance} ETH`);
     if (balance < 0.01) {
         throw new Error("Not enough ether");
-    }
+    };
 
     // Attaching the smart contract using Typechain
     const ballotFactory = new Ballot__factory(wallet);
-    const ballotContract = await ballotFactory.attach(contractAddress) as Ballot;
-    const tx = ballotContract.vote(proposlaNumber);
-    const receipt = await (await tx).wait();
-    console.log(`Transaction completed ${receipt?.hash}`)
-          // TODO
-}
+    const ballotContract = ballotFactory.attach(contractAddress) as Ballot;
+    const tx = await ballotContract.vote(proposalNumber);
+    const receipt = await tx.wait();
+    console.log(`Transaction completed ${receipt?.hash}`);
+};
 
 main().catch((error) => {
     console.error(error); 
--- scripts/deployWithEthers.ts
+++ scripts/deployWithEthers.ts
@@ -4,42 +4,51 @@ dotenv.config();
 import { Ballot__factory } from "../typechain-types";
 
 
-async function main() 
-{ 
-    // Receiving the parameters
-    const proposals = process.argv.slice(2)
-    if (!proposals || proposals.length < 1)
-      throw new Error("Proposals not provided");
-    console.log("Deploying Ballot contract"); 
-    console.log("Proposals: ");
-    proposals.forEach((element, index) => {
-        console.log(`Proposal N. ${index + 1}: ${element}`);
-    });    
+async function main() { 
+  // Receiving parameters
+  const proposals = process.argv.slice(2);
+  if (!proposals || proposals.length < 1)
+    throw new Error("Proposals not provided");
+  console.log("~~~~~~~~~~~~~ Deploying Ballot contract ~~~~~~~~~~~~~");
+  console.log("Proposals: ");
+  proposals.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
 
-    // Configuring the wallet
-    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
-    console.log(`Using address ${wallet.address}`);
-    const balanceBN = await provider.getBalance(wallet.address);
-    const balance = Number(ethers.formatUnits(balanceBN));
-    console.log(`Wallet balance ${balance} ETH`);
-    if (balance < 0.01) {
-        throw new Error("Not enough ether");
-    }
+  // Configuring the provider
+  // Can connect to any blockchain using the default provider with the line below
+  // const provider = ethers.getDefaultProvider("sepolia") 
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+  const lastBlock = await provider.getBlock('latest');
+  const lastBlockNumber = lastBlock?.number;
+  console.log(`Last block number was ${lastBlockNumber}`);
+  const lastBlockTimestamp = lastBlock?.timestamp ?? 0;
+  const lastBlockDate = new Date(lastBlockTimestamp * 1000);
+  console.log(`Last block timestamp: ${lastBlockTimestamp} (${lastBlockDate.toLocaleDateString()} ${lastBlockDate.toLocaleTimeString()})`);
 
-    // Deploying the contract
-    const ballotFactory = new Ballot__factory(wallet);
-    const ballotContract = await ballotFactory.deploy(
+  // Configuring the wallet
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider)
+  console.log(`Using address ${wallet.address}`);
+  const balanceBN = await provider.getBalance(wallet.address);
+  const balance = Number(ethers.formatUnits(balanceBN));
+  console.log(`Wallet balance ${balance} ETH`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+
+  // Deploying the smart contract using Typechain
+  const ballotFactory = new Ballot__factory(wallet);
+  const ballotContract = await ballotFactory.deploy(
     proposals.map(ethers.encodeBytes32String)
-    );
-    await ballotContract.waitForDeployment();
-    console.log(`Contract deployed to ${ballotContract.target}`);
-    for (let index = 0; index < proposals.length; index++) {
+  );
+  console.log('Waiting for deployment...')
+  await ballotContract.waitForDeployment();
+  console.log(`Contract deployed to ${ballotContract.target}`);
+  for (let index = 0; index < proposals.length; index++) {
     const proposal = await ballotContract.proposals(index);
     const name = ethers.decodeBytes32String(proposal.name);
     console.log({ index, name, proposal });
-    }
-          // TODO
+  }
 }
 
 main().catch((error) => {
